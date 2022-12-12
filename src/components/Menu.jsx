5b9311fdb56a7a184754016519f5be86322dd016
Artem Archenkov <r203mail@gmail.com>
2022-12-12 17:57:48 +0400

    v1.0.0

---
+++ src/components/Menu.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+
+function Menu() {
+  return (
+    <nav className="menu">
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? 'menu__item menu__item-active' : 'menu__item'
+        }
+        to="/" >Главная</NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? 'menu__item menu__item-active' : 'menu__item'
+        }
+        to="/drift" >Дрифт-такси</NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? 'menu__item menu__item-active' : 'menu__item'
+        }
+        to="/timeattack" >Time Attack</NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? 'menu__item menu__item-active' : 'menu__item'
+        }
+        to="/forza" >Forza Karting</NavLink>
+    </nav>
+  );
+}
+
+export default Menu;
